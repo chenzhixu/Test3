54daf93dab741f7772338e4503f8da92db5e50fa
chenzhixu <1532411082@qq.com>
2020-09-14 20:54:44 +0800

    test

---
+++ src/test/java/com/cy/Test3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
